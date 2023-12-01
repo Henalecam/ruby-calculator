c0484932344e726dba688a41ce07446f7653b385
Henrique Alexandre <henriquealexandredec@gmail.com>
2023-11-30 21:21:28 -0300

    Made Edit to Ruby on Rails Doc Test
    Signed-off-by: Henrique Alexandre <henriquealexandredec@gmail.com>

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,6 +21,19 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article #!These shortcuts it's a lot of time saving
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
     def article_params
       params.require(:article).permit(:title, :body)
